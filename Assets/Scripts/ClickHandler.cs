36838796fe081521ec0e6c8ae9589944c0ccbd9a
Guoxing.song <980538137@qq.com>
2015-11-02 16:11:58 +0800

    init

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		Button btn = GetComponent<Button> ();
+//		btn.onClick.AddListener (OnClick2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick()
+	{
+		Debug.Log("Button Clicked ");
+	}
+
+	private void OnClick2()
+	{
+		Debug.Log ("Button Clicked  2");
+	}
+}
---
+++ Assets/Scripts/state/IState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IState {
+
+	void OnEnter(string prevState);
+
+	void OnExit(string nextState);
+
+	void OnUpdate();
+}
